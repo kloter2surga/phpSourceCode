efbaaca726b9f448e0ae5784929bb96012b09f33
arfy slowy <slowy.arfy@gmail.com>
2021-01-01 21:47:29 +0700

    generate random string

---
+++ string/generateRandomString.php
@@ -0,0 +1,31 @@
+<?php
+function selection_sort(array $list)
+{
+    $newList = [];
+
+    $listSize = sizeof($list);
+
+    for ($i = 0; $i < $listSize; $i++) {
+        $smallest = find_smallest($list);
+        array_push($newList, $list[$smallest]);
+        array_splice($list, $smallest, 1);
+    }
+
+    return $newList;
+}
+function find_smallest(array $list)
+{
+    $smallest = $list[0];
+    $smallestIndex = 0;
+
+    foreach ($list as $key => $item) {
+        if ($list[$key] < $smallest) {
+            $smallest = $list[$key];
+            $smallestIndex = $key;
+        }
+    }
+
+    return $smallestIndex;
+}
+
+var_dump(selection_sort([5, 3, 6, 2, 10])); // => [2, 3, 5, 6, 10]
