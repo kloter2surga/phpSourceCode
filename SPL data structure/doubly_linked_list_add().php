ca9c4169e353f9bb1b3bd9c1129cbab263127937
khaira nabila <75076265+khairanabila@users.noreply.github.com>
2021-01-02 17:44:01 +0700

    doubly linked list add()
    ```php
    void SplDoublyLinkedList::add( $index, $newval )
    ```

---
+++ SPL data structure/doubly_linked_list_add().php
@@ -0,0 +1,17 @@
+?php  
+  
+// Declare an empty SplDoublyLinkedList 
+$list = new \SplDoublyLinkedList; 
+  
+// Use SplDoublyLinkedList::add() function to  
+// add elements to the SplDoublyLinkedList 
+$list->add(0, 1);  
+  
+$list->add(1, "KloterKnK");  
+  
+$list->add(2, "K");  
+  
+$list->add(3, 10);  
+  
+print_r($list);  
+?>  
